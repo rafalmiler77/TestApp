eb67680b344ab9b1e1e59551b51d7fc0fa12e1af
rafalmiler77 <rafalmiler77@gmail.com>
2017-08-31 22:41:43 +0200

    eslint added and some code refactoring

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
-var dataFile = require('./data/data.json')
+var dataFile = require('../client/data/data.json')
 
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 53764)
 
 app.get('/', function (req, res) {
-  res.send('<h1>rafael greets you</h1>')
+  res.send('<h1>rafael greets you, this is TestApp</h1>')
 })
 app.get('/test', function (req, res) {
   res.send('<h1>test</h1>')
