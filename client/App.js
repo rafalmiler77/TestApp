cbac5d9e2ca4bde6721152c212e90d72ac5592ba
rafalmiler77 <rafalmiler77@gmail.com>
2017-09-04 20:27:04 +0200

    fix double declaration in package.json

--- client/App.js
+++ client/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return <div>
     <h3>This is root component in react!!</h3><br />
-    Root document is hosted at '/'
+    <h4>Root document is hosted at '/'</h4>
     Root document in other version is hosted at '/client'
     Other paths: /admin, /test
 
